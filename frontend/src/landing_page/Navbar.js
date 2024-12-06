700223046b1d5ef24ad30aa9b9d8a954761dd3bb
KaranSJ22 <karanjamkhandi@gmail.com>
2024-12-06 14:14:17 +0530

    Navbar

--- frontend/src/landing_page/Navbar.js
+++ frontend/src/landing_page/Navbar.js
@@ -1,10 +1,24 @@
 import React from 'react';
-
+import './Navbar.css';
+// import Logo  from '../assets/logo.svg';
+import Logo from '../assets/logo.svg';
 function Navbar() {
     return ( 
-        <>
-        <h1>NAVBAR</h1>
-        </>
+        <nav className='nav-bar'>
+            <div className='logo'>
+                <img src={Logo} className='img-logo'></img>
+                
+            </div>
+            <div className='nav-btns'>
+                <a className='nav-btn'>Signup</a>
+                <a className='nav-btn'>About</a>
+                <a className='nav-btn'>Products</a>
+                <a className='nav-btn'>Pricing</a>
+                <a className='nav-btn'>Support</a>
+                <a className='nav-btn ham'><i class="fa-solid fa-bars"></i></a>
+
+            </div>
+        </nav>
      );
 }
 
---
+++ frontend/src/landing_page/pricing/PricingPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Hero from './Hero';
+import Brokerage from './Brokerage';
+
+function PriceingPage() {
+    return ( 
+        <>
+        <h1>Pricing Page</h1>
+        <Hero/>
+        <Brokerage/>
+        </>
+     );
+}
+
+export default PriceingPage;
